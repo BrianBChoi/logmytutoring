ef14f590eeb16722f284f88d7401311061efe08d
Brian <brianccodes@gmail.com>
2020-10-22 01:22:44 -0700

    Fix shell context processor function

--- microblog.py
+++ microblog.py
@@ -3,4 +3,4 @@ from app.models import User, Student, Session
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Session': Session, 'Student', Student}
+    return {'db': db, 'User': User, 'Session': Session, 'Student': Student}
