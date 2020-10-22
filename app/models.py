fd9fe09c417cbf1881134a043edd740225196859
Brian <brianccodes@gmail.com>
2020-10-22 01:11:22 -0700

    Add students table

--- app/models.py
+++ app/models.py
@@ -7,5 +7,16 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    students = db.relationship('Student', backref='tutor', lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True)
+    hourly_rate = db.Column(db.Float, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Student {}>'.format(self.name)
