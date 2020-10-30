d4c1bf2ff9df7266fb9665fc5b88ca45a35887a7
Brian <brianccodes@gmail.com>
2020-10-29 23:33:19 -0700

    Unit test new_sessions()

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
-from app.models import User
+from app.models import User, Session
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -23,6 +23,31 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
+
+    def test_new_sessions(self):
+        # create a user
+        u = User(username='john')
+        db.session.add(u)
+        db.session.commit()
+        self.assertEqual(u.sessions.all(), [])
+
+        # create three sessions
+        s1 = Session(notes='session1', tutor=u,
+                     date=datetime.utcnow() + timedelta(seconds=1))
+        s2 = Session(notes='session2', tutor=u,
+                     date=datetime.utcnow() + timedelta(seconds=2))
+        s3 = Session(notes='session3', tutor=u,
+                     date=datetime.utcnow() + timedelta(seconds=3))
+        db.session.add_all([s1, s2, s3])
+        db.session.commit()
+
+        # check all sessions are showing up
+        all_sessions = u.sessions.all()
+        self.assertEqual(all_sessions, [s1, s2, s3])
+
+        # check descending order of new_sessions()
+        new_sessions = u.new_sessions().all()
+        self.assertEqual(new_sessions, [s3, s2, s1])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
